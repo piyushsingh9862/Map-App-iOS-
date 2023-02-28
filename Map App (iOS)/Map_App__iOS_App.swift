1b2c784f8db514f97d8ec2a9b3e17e76e03174ba
PIYUSH SINGH <piyushsingh9862@gmail.com>
2023-02-28 16:30:30 +0530

    Initial Commit

---
+++ Map App (iOS)/Map_App__iOS_App.swift
@@ -0,0 +1,17 @@
+//
+//  Map_App__iOS_App.swift
+//  Map App (iOS)
+//
+//  Created by Piyush Singh on 28/02/23.
+//
+
+import SwiftUI
+
+@main
+struct Map_App__iOS_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
